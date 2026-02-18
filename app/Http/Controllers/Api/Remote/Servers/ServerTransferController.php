a25e287fcd5c926fbe3e49df2c01cb65f1ad6cbb
Jackinatox <jakob.schulze1206@gmail.com>
2026-02-18 12:08:57 +0100

    Refactor ServerTransferController to use Request object and improve node validation

--- app/Http/Controllers/Api/Remote/Servers/ServerTransferController.php
+++ app/Http/Controllers/Api/Remote/Servers/ServerTransferController.php
@@ -2,7 +2,10 @@
 
 namespace Pterodactyl\Http\Controllers\Api\Remote\Servers;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Webmozart\Assert\Assert;
+use Pterodactyl\Models\Node;
 use Illuminate\Http\JsonResponse;
 use Pterodactyl\Models\Allocation;
 use Illuminate\Support\Facades\Log;
@@ -32,8 +35,10 @@ class ServerTransferController extends Controller
      *
      * @throws \Throwable
      */
-    public function failure(string $uuid): JsonResponse
+    public function failure(Request $request, string $uuid): JsonResponse
     {
+        Assert::isInstanceOf($node = $request->attributes->get('node'), Node::class);
+
         $server = $this->repository->getByUuid($uuid);
         $transfer = $server->transfer;
         if (is_null($transfer)) {
@@ -42,7 +47,7 @@ class ServerTransferController extends Controller
 
         // Either node can tell the panel that the transfer has failed. Only the new node
         // can tell the panel that it was successful.
-        if (! $server->node->is($transfer->newNode) && ! $server->node->is($transfer->oldNode)) {
+        if ($node->id !== $transfer->new_node && $node->id !== $transfer->old_node) {
             throw new HttpForbiddenException('Requesting node does not have permission to access this server.');
         }
 
@@ -54,8 +59,10 @@ class ServerTransferController extends Controller
      *
      * @throws \Throwable
      */
-    public function success(string $uuid): JsonResponse
+    public function success(Request $request, string $uuid): JsonResponse
     {
+        Assert::isInstanceOf($node = $request->attributes->get('node'), Node::class);
+
         $server = $this->repository->getByUuid($uuid);
         $transfer = $server->transfer;
         if (is_null($transfer)) {
@@ -64,7 +71,7 @@ class ServerTransferController extends Controller
 
         // Only the new node communicates a successful state to the panel, so we should
         // not allow the old node to hit this endpoint.
-        if (! $server->node->is($transfer->newNode)) {
+        if ($node->id !== $transfer->new_node) {
             throw new HttpForbiddenException('Requesting node does not have permission to access this server.');
         }
 
